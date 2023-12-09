20076c4e799b743842d40f5ec0aa174495df3460
rustem.abd <rustem.abd@mail.ru>
2023-12-09 04:06:51 +0300

    второе исправление

--- routes/admin.js
+++ routes/admin.js
@@ -5,25 +5,17 @@ const db = require('../db')
 const path = require('path')
 const fs = require('fs')
 
-
 // const skills = db.get('skills').value()
 // const products = db.get('products').value()
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res, next) => {  
   // TODO: Реализовать, подстановку в поля ввода формы 'Счетчики'
   // актуальных значений из сохраненых (по желанию)
-  // db.update('skills', (skills) => {
-  //   return skills.map((skill) => {
-  //     if (skill.id === 0) {
-  //       return { ...skill, number: '' }
-  //     }
-  //     return skill
-  //  }).write()
-  // }) 
-  const msgskill = req.flash('msgskill')[0]
-  const msgfile = req.flash('msgfile')[0]
+  // const savedSkill = db.get('skills').value()
+  // const savedFiles = db.get('files').value() savedSkill, savedFiles,
+  // ,
 
-  res.render('pages/admin', { title: 'Admin page', msgskill, msgfile }) 
+  res.render('pages/admin', { title: 'Admin page', msgskill: req.flash('message')[0], msgfile: req.flash('message')[0] }) 
 })
 
 router.post('/skills', (req, res, next) => {
@@ -35,17 +27,16 @@ router.post('/skills', (req, res, next) => {
     в переменной cities - Максимальное число городов в туре
     в переменной years - Лет на сцене в качестве скрипача
   */
-    const { number, text } = req.body
-    const newSkill = []
-    newSkill.push({
-      number: Number(number),
-      text: text
-    })
-    for (let i = 0; i < newSkill.length; i++) {
-      newSkill[i].number += 1; 
-    }
+    const formData = req.body
+    Object.keys(formData).forEach((key) => {
+      db.get('skills')
+      .find({ type: key })
+      .assign({ number: formData[key] }) // Замените 'новое значение' на актуальное значение
+      .write();
+    })  
   
-    req.flash('msgskill')
+    req.flash('message', 'Навыки обновленны')
+    res.redirect('/admin')
 })
 
 router.post('/upload', (req, res, next) => {
@@ -57,45 +48,54 @@ router.post('/upload', (req, res, next) => {
     На текущий момент эта информация хранится в файле data.json  в массиве products
   */
   const form = new formidable.IncomingForm()
-  const upload = path.join('./upload')
+  form.uploadDir = path.join(__dirname, '../', 'upload')
+  // const upload = path.join('./upload')
 
-  if (!fs.existsSync(upload)) {
-    fs.mkdirSync(upload)
+  if (!fs.existsSync(form.uploadDir)) {
+    fs.mkdirSync(form.uploadDir)
   }
 
   form.parse(req, (err, fields, files) => {
     if (err) {
       console.log(err);
-      res.send('Ошибка при загрузке файла');
-      return;
+      req.flash('message', 'Ошибка при загрузке файла');
+      return res.redirect('/admin')
     }
 
     const { name, price } = fields
-    const { photo } = files
+    const {filepath, originalFilename }= files.photo[0]
+    console.log(fields)
+    console.log(files)
+    // const { photo } = files
     const valid = validation(fields, files)
     if (valid.err) {
-      fs.unlinkSync(files.photo.path)
-      return res.redirect(req.flash('msgfile'))
+      fs.unlinkSync(filepath)
+      req.flash('message', valid.status)
+      return res.redirect('/admin')
+    } else {
+      fs.renameSync(filepath, path.join(form.uploadDir, originalFilename))
     }
 
     const newProduct = {
-      name: name,
+      name: name[0],
       price: Number(price),
-      photo: photo.path, // Путь к загруженному файлу 
+      src: `/${originalFilename}`, // Путь к загруженному файлу 
     }
 
     db.get('products')
       .push(newProduct)
       .write()   
   })
-  req.flash('msgfile')
+  req.flash('message', 'Файлы успешно добавлены')
+  res.redirect('/admin')
 })
 
 const validation = (fields, files) => {
-  if (!files.photo) {
+  const { photo } = files
+  if (!photo || photo.length === 0) {
     return { status: 'Не загружена картинка!', err: true }
   }
-  if (!fields.name) {
+  if (!fields.name || fields.name.length === 0 || !fields.price || fields.price.length === 0) {
     return { status: 'Не указано описание картинки!', err: true }
   }
   return { status: 'Ok', err: false }
